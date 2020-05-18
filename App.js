0bdf735ecff0ca39bc122439828b75628cfa6dd7
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-05-18 00:16:10 -0400

    Add text inputs for credentials.

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
 import { CheckAuthentication, Authenticate } from './authentication/Authentication';
 
 async function authService(email, password){ 
-  return { };
+  if (email == "test@email.com" && password == "Test1234") {
+    return {};
+  } else {
+    return { error: "Invalid credentials" };
+  }
 };
 
-async function onPress(state, setState) {
-  let authState = await Authenticate("test", "pw", authService);
-  setState(Object.assign({}, state, authState));
+async function onPress(username, password, setState) {
+  let authState = await Authenticate(username, password, authService);
+  setState(authState);
 };
 
 function renderAuthenticated() {
   return <Text style={styles.instructions}>Yep</Text>;
 };
 
-function renderUnauthenticated(state, setState) {
-  return <Button title="Login" onPress={() => onPress(state, setState)}></Button>;
+function renderUnauthenticated(state, setState, username, setUsername, password, setPassword) {
+  return <View>
+    <TextInput style={styles.inputs} value={username} onChangeText={text => setUsername(text)} />
+    <TextInput style={styles.inputs} value={password} onChangeText={text => setPassword(text)} />
+    <Button title="Login" onPress={() => onPress(username, password, setState)} />
+  </View>;
 };
 
 function renderError(state) {
@@ -27,10 +35,13 @@ let initialAuthState = CheckAuthentication();
 
 export default function App() {
   let [state, setState] = React.useState(initialAuthState);
+  let [username, setUsername] = React.useState("username");
+  let [password, setPassword] = React.useState("password");
 
   return (
     <View style={styles.container}>
-      {state.authenticated ? renderAuthenticated() : renderUnauthenticated(state, setState)}
+      
+      {state.authenticated ? renderAuthenticated() : renderUnauthenticated(state, setState, username, setUsername, password, setPassword)}
       {state.error ? renderError(state) : null}
     </View>
   );
@@ -53,4 +64,10 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  inputs: {
+    height: 40,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
